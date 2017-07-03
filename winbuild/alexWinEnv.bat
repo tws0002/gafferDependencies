b423f26d9629108996a006466b6f71241bc6395d
Alex Fuller <boberfly@gmail.com>
2017-07-02 18:20:23 -0700

    Temp batch files.

---
+++ winbuild/alexWinEnv.bat
@@ -0,0 +1,7 @@
+set BUILD_DIR=C:\Users\Alex\dev\gafferDependenciesInstall
+set VERSION=0.33.1.0
+set ARNOLD_ROOT=C:\arnold\4.2.14.1\
+rem set RMAN_ROOT=C:/Program\ Files/3Delight\ For\ Maya/
+set RMAN_ROOT=C:\delight\
+set CMAKE_GENERATOR="Visual Studio 12 2013 Win64"
+set ROOT_DIR=C:\Users\Alex\dev\dependencies\winbuild
