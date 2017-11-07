3b11135e97ddf0ed9ecfbedf368f1d3d44a0ded1
Eric Mehl <eric@thinkhypothetical.com>
2017-11-07 15:57:55 -0500

    change python paths to lib64 so they can be found by boost

--- winbuild/buildBoost.bat
+++ winbuild/buildBoost.bat
@@ -7,7 +7,7 @@ rem give a helping hand to find the python headers, since the bootstrap below do
 rem always seem to get it right.
 set GAFFER_PYTHONPATH=%BUILD_DIR%
 
-set PYTHONPATH=%BUILD_DIR%
+set PYTHONPATH=%BUILD_DIR%/lib64
 
 call bootstrap.bat --prefix=%BUILD_DIR% --with-python=%BUILD_DIR%\bin\python --with-python-root=%BUILD_DIR% --without-libraries=log
 b2 --prefix=%BUILD_DIR% --toolset=%BOOST_MSVC_VERSION% architecture=x86 address-model=64 --build-type=complete variant=release link=shared threading=multi -s ZLIB_SOURCE=%ROOT_DIR%\..\zlib-1.2.11 -s ZLIB_INCLUDE=%BUILD_DIR%\include -s ZLIB_LIBPATH=%BUILD_DIR%\lib -s ZLIB_BINARY=zlib install
