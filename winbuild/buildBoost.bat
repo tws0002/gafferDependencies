79fa41c85ef747d45e611a3c7a74183a53cc4400
Eric Mehl <eric@thinkhypothetical.com>
2017-11-17 20:57:46 -0500

    fix boost to find the right include files and libraries

--- winbuild/buildBoost.bat
+++ winbuild/buildBoost.bat
@@ -3,13 +3,13 @@ cd %~dp0%..\boost_1_61_0
 mkdir %BUILD_DIR%\doc\licenses
 copy LICENSE_1_0.txt %BUILD_DIR%\doc\licenses\boost
 
-rem give a helping hand to find the python headers, since the bootstrap below doesn't
-rem always seem to get it right.
-set GAFFER_PYTHONPATH=%BUILD_DIR%
+set PYTHONPATH=%BUILD_DIR%;%BUILD_DIR%\bin;%BUILD_DIR%\lib64;%BUILD_DIR%\lib
+set SAVEPATH=%PATH%
+set PATH=%PATH%;M:\gaffer\bin
 
-set PYTHONPATH=%BUILD_DIR%/lib64
-
-call bootstrap.bat --prefix=%BUILD_DIR% --with-python=%BUILD_DIR%\bin\python --with-python-root=%BUILD_DIR% --without-libraries=log
+call bootstrap.bat --prefix=%BUILD_DIR% --with-python=%BUILD_DIR% --with-python-root=%BUILD_DIR% --without-libraries=log
 b2 --prefix=%BUILD_DIR% --toolset=%BOOST_MSVC_VERSION% architecture=x86 address-model=64 --build-type=complete variant=release link=shared threading=multi -s ZLIB_SOURCE=%ROOT_DIR%\..\zlib-1.2.11 -s ZLIB_INCLUDE=%BUILD_DIR%\include -s ZLIB_LIBPATH=%BUILD_DIR%\lib -s ZLIB_BINARY=zlib install
 
 cd %ROOT_DIR%
+
+set PATH=%SAVEPATH%
--- winbuild/buildSubprocess32.bat
+++ winbuild/buildSubprocess32.bat
@@ -1,5 +1,5 @@
 cd %~dp0%..\subprocess32-3.2.6
 
-copy subprocess32.py %BUILD_DIR%\\lib\\site-packages
+copy subprocess32.py %BUILD_DIR%\lib\site-packages
 
 cd %ROOT_DIR%
