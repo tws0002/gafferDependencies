2211e4cdbcd9cf2d267c7236140cbd2b545e0a18
Eric Mehl <eric@thinkhypothetical.com>
2018-04-15 16:30:38 -0400

    fix up subprocess a little more to make it more direct create Python directory so subprocess copy doesn't fail

--- winbuild/buildSubprocess32.bat
+++ winbuild/buildSubprocess32.bat
@@ -1,7 +1,6 @@
-cd %ROOT_DIR%\..\subprocess32-3.2.6
-
 rem copy subprocess32.py %BUILD_DIR%\lib64\site-packages\subprocess32.py
-copy subprocess32.py %BUILD_DIR%\python\subprocess32.py
+mkdir %BUILD_DIR%\python
+copy %ROOT_DIR%\subprocess32-3.2.6\subprocess32.py %BUILD_DIR%\python\subprocess32.py
 if %ERRORLEVEL% NEQ 0 (exit /b %ERRORLEVEL%)
 
 cd %ROOT_DIR%
