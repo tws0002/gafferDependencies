832950635120c6e4d16a78baf0ff1c7afc3bf6e2
Eric Mehl <eric@thinkhypothetical.com>
2018-04-15 16:09:37 -0400

    fix typo

--- winbuild/buildSubprocess32.bat
+++ winbuild/buildSubprocess32.bat
@@ -1,4 +1,4 @@
-cd ROOT_DIR\..\subprocess32-3.2.6
+cd %ROOT_DIR%\..\subprocess32-3.2.6
 
 rem copy subprocess32.py %BUILD_DIR%\lib64\site-packages\subprocess32.py
 copy subprocess32.py %BUILD_DIR%\python\subprocess32.py
